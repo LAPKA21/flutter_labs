f5e06bf1a2d2664ce5a3c8bec196a0158d2e9c20
LAPKA21 <01d.schoolcyrax916@gmail.com>
2025-03-11 19:24:38 +0900

    Добавляю лабораторную работу номер 2

---
+++ flutter_application_lab_2/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Лабораторная работа №2 Богушевич Д.А. ВМК-22 вар(2)'),
+        ),
+        body: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[ 
+          Column(
+            children: [
+              //В  задании сказано добавить текстовые виджеты ик каждому текстовому виджету добавить стиль и отступ. Отступ сделан с помощью padding это виджет, который добавляет отступы (поля) вокруг своего дочернего элемента. Он используется, чтобы задать расстояние между виджетами или их границами.
+              Padding(padding: EdgeInsets.all(16.0), 
+              child: Text('Фио: Богушевич Данил Александрович', style: TextStyle(fontSize: 30, color: Color.fromRGBO(3, 252, 198, 1.0) ))),
+              Padding(padding: EdgeInsets.all(16.0),
+              child: Text('Группа  обучения: ВМК-22 ', style: TextStyle(fontSize: 25,color: Color.fromRGBO(3, 252, 45, 1.0)),)),
+              Padding(padding: EdgeInsets.all(16.0),
+              child: Text('Год рождения: 2004', style: TextStyle(fontSize: 20, color: Color.fromRGBO(3, 119, 252, 1.0))))
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
