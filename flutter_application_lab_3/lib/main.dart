97b6a260ac5469f3c44dbfc4f4a1d4ae5985b439
LAPKA21 <01d.schoolcyrax916@gmail.com>
2025-03-18 13:03:08 +0900

    Добавил разделение экранов по файлам

--- flutter_application_lab_3/lib/main.dart
+++ flutter_application_lab_3/lib/main.dart
@@ -1,4 +1,4 @@
-import 'dart:math';
+import 'Result.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart'; // Импорт для inputFormatters
@@ -96,35 +96,5 @@ Widget build(BuildContext  context){
 }
 
 
-class SecondScreen extends StatelessWidget {
-  final String numA;
-  final String numB;
 
-  const SecondScreen({super.key, required this.numA, required this.numB});
-
-  @override
-  Widget build(BuildContext context) {
-    num sum = pow(int.parse(numA),2) + pow(int.parse(numB),2);
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Результат'),
-      ),
-      body: Column(
-         mainAxisAlignment: MainAxisAlignment.center, //Цетрирование
-        children: <Widget>[ 
-          Padding(padding: EdgeInsets.all(16.0), child:  Text(
-          'Сумма $numA + $numB = $sum',
-          style: const TextStyle(fontSize: 24),
-        )),
-          const SizedBox(height: 20.0),
-         ElevatedButton(onPressed: (){
-           Navigator.pop(context);
-         },
-         child: Text('Переход к калькулятору'),
-      )],
-        
-      ),
-    );
-  }
-}
 
